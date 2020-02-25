949e4e1ca5d83c694b802eea283dd3e0c58c8c5d
Santiago Vera <svera1213@gmail.com>
2020-02-24 20:00:28 -0500

    Initial Commit

---
+++ LongBlack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LongBlack
+//
+//  Created by Augusto Vera on 2/24/20.
+//  Copyright © 2020 longblack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
